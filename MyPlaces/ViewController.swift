1a1f815f5bca450e28a6d52bb5d558f2cc79f275
Marusin Victor <d_stuart@mail.ru>
2020-04-23 20:06:56 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Виктор Марусин on 23.04.2020.
+//  Copyright © 2020 Виктор Марусин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
