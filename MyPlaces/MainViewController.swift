4ba332cf0ad182de97281f9251f63b104d6b3f1a
Marusin Victor <d_stuart@mail.ru>
2020-05-08 17:32:11 +0300

    Реализовал функционал поиска

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -11,30 +11,57 @@ import RealmSwift
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    private var searchController = UISearchController(searchResultsController: nil) //свойство UISearchController для реализации поиска
+    //инициализируем нашу модель в которой есть функфия заполнения модели данными. nil означает что для отображения результатов поиска мы не хотим создавать новый view а отображаем его во view где происходит поиск
+    private var places: Results<Place>! //Results это автообновляемы тим контейнера который возвращает запрашиваемые обьекты (аналог массива но для БД)
+    private var filteredPlaces: Results<Place>! // в коллекцию будем помещать отфильтрованные записи
+    private var ascendingSorting = true //свойство отвечающее за обратную сортировку
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else  { return false }// возвращает true если строка будет пустой
+        return text.isEmpty
+    }
+    
+    private var isFiltering: Bool {
+        return searchController.isActive && !searchBarIsEmpty
+    }
+    
     @IBOutlet var tableView: UITableView!
     @IBOutlet var segmentedControl: UISegmentedControl!
     @IBOutlet var reversedSortingButton: UIBarButtonItem!
     
-    //инициализируем нашу модель в которой есть функфия заполнения модели данными
-    var places: Results<Place>! //Results это автообновляемы тим контейнера который возвращает запрашиваемые обьекты (аналог массива но для БД)
-    var ascendingSorting = true //свойство отвечающее за обратную сортировку
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         places = realm.objects(Place.self) //отображаем данные БД на экране инициализировав обьект places (запрашиваем данные в БД)
+        
+        //здесь производим настройку UISearchController
+        searchController.searchResultsUpdater = self  //указываем что получателем информации об изменении текстов в поисковой строке будет наш класс
+        searchController.obscuresBackgroundDuringPresentation = false  //VC с результатами поиска не позволяет взаимодействовать с отображаемым. Мы это выключаем и работаем как с основным (можем смотреть результаты исправлять их и удалять)
+        searchController.searchBar.placeholder = "Search" //указываем отображение плейсхолдера строки поиска
+        navigationItem.searchController = searchController// строка поиска будет интегрирована в navigationbar
+        definesPresentationContext = true //позволяет отпустить строку поиска при переходе на другой экран!!
     }
 
     // MARK: - Table view data source
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            return filteredPlaces.count
+        }
         return places.isEmpty ? 0 : places.count // кол-во ячеек равно кол-ву эл БД. Если БД пустая то возырвщает пусто
     }
     //наполнение ячеек
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell // as! CustomTableViewCell вставили специльно что бы привести к типу нашеve классу CustomTableViewCell
 
-        let place = places[indexPath.row]
+        var place = Place()
+        
+        if isFiltering {
+            place = filteredPlaces[indexPath.row]
+        } else {
+            place = places[indexPath.row]
+        }
 
         // обращаемся к конкретному обьекту из массива places и передаем в лейбл CustomTableViewCell
         cell.nameLabel.text = place.name
@@ -50,16 +77,6 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     // MARK: - Table View delegate
     
     // в этом метод мы помещаем все действия доступны после свайпа строки влево
-//    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-//        let place = places[indexPath.row] //опрделяем обьект для удадления
-//        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (_, _) in
-//
-//            StorageManager.deleteObject(place)
-//            tableView.deleteRows(at: [indexPath], with: .automatic)
-//        }
-//        return [deleteAction]
-//    }
-    
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
         let place = places[indexPath.row] //опрделяем обьект для удадления
@@ -83,7 +100,12 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             guard let indexPath = tableView.indexPathForSelectedRow else { return }// определяем индекс текущий ячейки
-            let place = places[indexPath.row]//имея индекс текущей строки  мы можем извлечь обьект из массива Places по этому индексу
+            let place: Place
+            if isFiltering {
+                place = filteredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
             let newPlaceVC = segue.destination as! NewPlaceViewController // создаем экземлпяр этого VC
             newPlaceVC.currentPlace = place//обращаемся к нашему экземпляру и передаем ему значение place
         }
@@ -121,6 +143,18 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
             places = places.sorted(byKeyPath: "name", ascending: ascendingSorting)
         }
         
+        tableView.reloadData()
+    }
+}
+
+extension MainViewController: UISearchResultsUpdating {
+    // метод фильтации контента в соответствии с запросом
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearhText(searchController.searchBar.text!)
+    }
+    
+    private func filterContentForSearhText(_ searchText: String) {
+        filteredPlaces  = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@", searchText, searchText)//заполняем коллекцию отфильтрованными обьектами массива Places. Фильтруем по значанению. CONTAINS[c] - не смотрим на регистр символов
         tableView.reloadData()
     }
 }
